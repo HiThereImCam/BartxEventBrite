05c07e4ca0d8664bf3b53a2c0ea066aa91cd7904
Cameron Tanjoco <ctanjoco@gmail.com>
2021-12-29 21:08:47 -0800

    Revert "added api url to .env and added error checking to api call:"
    This reverts commit 90ec03b3efcab58485b5e26c7de8c36eb105d9d0.

--- src/Components/util/getData.tsx
+++ src/Components/util/getData.tsx
@@ -1,23 +1,17 @@
 import checkErrorStatus from "./checkErrorStatus";
 
-const getData = async (url: any) => {
-  try {
-    const dataResponse = await fetch(url);
-    let error = checkErrorStatus(dataResponse);
-    if (error !== null) {
-      console.log("HERE");
-      throw new Error(error);
-    } else {
-      const data = await dataResponse.json();
-      return data;
-    }
-  } catch (err) {
-    if (err instanceof Error) {
-      return err.message;
-    } else {
-      return String(err);
-    }
+const getData = async (url: string) => {
+  const dataResponse = await fetch(url);
+
+  let error = checkErrorStatus(dataResponse);
+
+  if (error !== null) {
+    throw new Error(error);
   }
+
+  const data = await dataResponse.json();
+
+  console.log("data: ", data);
 };
 
 export default getData;
